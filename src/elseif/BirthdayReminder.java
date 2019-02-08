448d6d7ba865299d8686daebb3741f79cb0af3a6
Cjrocketproof12 <league@iMac-6.attlocal.net>
2019-02-07 16:59:23 -0800

    BirthdayReminder(IP)

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -13,18 +13,21 @@ public class BirthdayReminder {
 		// 1. correct the birthdays for your family below
 		String momsBirthday = "January 24th";
 		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String myBirthday = "April 19th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	JOptionPane.showInputDialog("whos birthday");
 		// 3. Print out what the user typed
 		
 		// 4. if user asked for "mom"
 			//print mom's birthday
+
 		// 5. if user asked for "dad"
 			// print dad's birthday
+		
 		// 6. if user asked for your name
 			// print myBirthday
+		
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
 
 	} 
