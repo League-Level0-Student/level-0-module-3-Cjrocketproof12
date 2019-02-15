62a907f3d55da79fc1cc4714c24100dc75814c3a
Cjrocketproof12 <league@WTS-IM-6.attlocal.net>
2019-02-14 16:54:10 -0800

    Birthday reminder, crazycatlady, highlowgame(IP)

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -9,26 +9,40 @@ import javax.swing.JOptionPane;
 public class BirthdayReminder {
 
 	public static void main(String[] args) {
-	
+
 		// 1. correct the birthdays for your family below
 		String momsBirthday = "January 24th";
 		String dadsBirthday = "June 9th";
-		String myBirthday = "April 19th";
+		String myBirthday = "aokrls 09919291th";
 
-		// 2. Find out which birthday the user wants and and store their response in a variable
-	JOptionPane.showInputDialog("whos birthday");
+		// 2. Find out which birthday the user wants and and store their response in a
+		// variable
+		String str = JOptionPane.showInputDialog("whos birthday");
 		// 3. Print out what the user typed
-		
+
+		System.out.println(str);
 		// 4. if user asked for "mom"
-			//print mom's birthday
+		// print mom's birthday
+		if (str.equals("mom")) {
+			JOptionPane.showMessageDialog(null, "June 928228th");
 
+		}
 		// 5. if user asked for "dad"
-			// print dad's birthday
-		
-		// 6. if user asked for your name
-			// print myBirthday
-		
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
+		// print dad's birthday
+		else if (str.equals("dad")) {
+			JOptionPane.showMessageDialog(null, "January 83788328th");
 
-	} 
+		}
+		// 6. if user asked for your name
+		// print myBirthday
+		else if (str.equals("your")) {
+			JOptionPane.showMessageDialog(null, "aprilahfiahifhahhfa 1978177117191919198197813th");
+
+		}
+		// 7. otherwise print "Sorry, i don't remember that person's birthday!"
+		else {
+			JOptionPane.showMessageDialog(null, "dunno");
+
+		}
+	}
 }
--- src/extra/DizzyRobot.java
+++ src/extra/DizzyRobot.java
@@ -7,12 +7,13 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
 	static Robot robot = new Robot();
-	public static void main(String[] args) {
-		
-		 // 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
 
- // 1. Use the dance method to make the robot spin.
-	
+	public static void main(String[] args) {
+
+		// 2. Ask the user how dizzy you want the robot from 1-10, then spin that number
+		// of times.
+
+		// 1. Use the dance method to make the robot spin.
 
 	}
 
@@ -22,4 +23,3 @@ public class DizzyRobot {
 		}
 	}
 }
-
