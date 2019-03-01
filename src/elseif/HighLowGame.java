99c501601d4e51c6e31643c4040059434e77c037
eliyahubowers <league@WTS-IM-6.attlocal.net>
2019-02-28 16:54:35 -0800

    HighLowGame, RobotColorChooser(IP)

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -10,29 +10,43 @@ import javax.swing.JOptionPane;
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
+		// 3. Change this line to give you a random number between 1 - 100.
 		int random = new Random().nextInt(100);
-		
+
 		// 2. Print out the random variable above
 		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
-		
-			// 1. Ask the user for a guess using a pop-up window, and save their response 
-String epic = JOptionPane.showInputDialog("wht ur guass?");
-			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
-			// 5. if the guess is correct
-				// 6. Win
-				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
-			// 7. if the guess is high
-				// 8. Tell them it's too high
-			// 9. if the guess is low
-				// 10. Tell them it's too low
+		for (int i = 0; i < 10; i++) {
 
+			// 1. Ask the user for a guess using a pop-up window, and save their response
+			String epic = JOptionPane.showInputDialog("wht ur guass?(Whats your guess?)");
+			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+			int epocnessasesaeafasfdhafe = Integer.parseInt(epic);
+			// 5. if the guess is correct
+			if (epocnessasesaeafasfdhafe == random) {
+				JOptionPane.showMessageDialog(null, "u won");
+
+			
+			// 6. Win
+			// 12. Use "System.exit(0);" to quit the game if the user guessed the right
+			// answer.
+			System.exit(random);
+			}
+			// 7. if the guess is high
+			if (epocnessasesaeafasfdhafe > random) {
+				JOptionPane.showMessageDialog(null, "ur guas is too big");
+
+			}
+			// 8. Tell them it's too high
+			// 9. if the guess is low
+			else if (epocnessasesaeafasfdhafe < random) {
+				JOptionPane.showMessageDialog(null, "ur to smol");
+
+			}
+			// 10. Tell them it's too low
+		}
 		// 13. Tell them they lose
-		
+		JOptionPane.showMessageDialog(null, "u lose");
 	}
 
 }
-
-
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,56 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
+import java.util.Random;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
+		Random ninjadiedofligma = new Random();
+		// 1. Create a new Robot
+		Robot pewDiePie = new Robot();
+		// 3. Ask the user what color they would like the robot to draw
+		String epic = JOptionPane.showInputDialog(null, "wut color red gren or blu");
+		// 5. Use an if/else statement to set the pen color that the user requested
+		if (epic.equals( "red")) {
+			pewDiePie.setPenColor(0, 255, 0);
+			
+		}
+		else if (epic.equals( "green")) {
+			pewDiePie.setPenColor(0, 0, 255);
+			
+		}
+		else if (epic.equals( "blue")) {
+			pewDiePie.setPenColor(255, 0, 0);
+			
+		}
+			;
+		// 6. If the user doesn’t enter anything, choose a random color
+			if(epic.equals("")) {
+				int red = ninjadiedofligma.nextInt(256);	 
+						int green =ninjadiedofligma.nextInt(256);
+								int blue =ninjadiedofligma.nextInt(256);
+				(new Color( , , );
+			
 		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
+			
+				pewDiePie.setPenColor(ninjadiedofligma);
+			}
+		// 7. Put a loop around your code so that you keep asking the user for more
+		// colors & drawing them
 
-        //6. If the user doesn’t enter anything, choose a random color
+		// 4. Set the pen width to 10
+		pewDiePie.setPenWidth(10);
+		// 2. Make the robot draw a shape (this will take more than one line of code)
+		for (int i = 0; i < 100000; i++) {
+			pewDiePie.setSpeed(1000);
+			pewDiePie.penDown();
+			pewDiePie.move(300);
+			pewDiePie.turn(90);
 
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+		}
 	}
 }
